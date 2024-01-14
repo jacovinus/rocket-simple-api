2593360dc41f4dae7513508a4e1b9705e78b1bbc
jacovinus <jacovinus@gmail.com>
2024-01-14 08:51:09 +0100

    add todo model

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,24 @@ extern crate rocket;
 
 #[derive(Serialize)]
 pub struct GenericResponse {
-
     pub status: String,
     pub message: String,
-
 }
 
 #[get("/healthchecker")]
-pub async fn health_checker_handler() -> Result<Json<GenericResponse> , Status> {
-
+pub async fn health_checker_handler() -> Result<Json<GenericResponse>, Status> {
     const MESSAGE: &str = "Build Simple CRUD API with Rust and Rocket";
 
     let response_json = GenericResponse {
-
-    status: "success".to_string(),
+        status: "success".to_string(),
         message: MESSAGE.to_string(),
-
-    
     };
     Ok(Json(response_json))
-
-
 }
 
 #[launch]
 fn rocket() -> _ {
-
     rocket::build().mount("/api", routes![health_checker_handler,])
-
 }
 
 /*
