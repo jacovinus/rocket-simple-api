c89c1e9f0c3ff54bd6a5ad6fbe430f25e3455a5c
jacovinus <jacovinus@gmail.com>
2024-01-14 12:29:52 +0100

    feat: create todo handler

--- src/handler.rs
+++ src/handler.rs
@@ -11,6 +11,7 @@ use rocket::{
 
 use uuid::Uuid;
 
+// GET health
 #[get("/health")]
 pub async fn health_check_handler() -> Result<Json<GenericResponse>, Status> {
     const MESSAGE: &str = "Build Simple CRUD API with Rust and Rocket";
@@ -22,6 +23,7 @@ pub async fn health_check_handler() -> Result<Json<GenericResponse>, Status> {
     Ok(Json(response_json))
 }
 
+// GET todos   get all todos
 #[get("/todos?<page>&<limit>")]
 pub async fn todos_list_handler(
     page: Option<usize>,
@@ -43,5 +45,44 @@ pub async fn todos_list_handler(
         todos,
     };
 
+    Ok(Json(json_response))
+}
+
+// POST todos   new todo
+#[post("/todos", data = "<body>")]
+pub async fn create_todo_handler(
+    mut body: Json<Todo>,
+    data: &State<AppState>,
+) -> Result<Json<SingleTodoResponse>, Custom<Json<GenericResponse>>> {
+    let mut vec = data.todo_db.lock().unwrap();
+
+    for todo in vec.iter() {
+        if todo.title == body.title {
+            let error_response = GenericResponse {
+                status: "fail".to_string(),
+                message: format!("Todo with title {} already exists", todo.title),
+            };
+            return Err(Custom(Status::Conflict, Json(error_response)));
+        }
+    }
+
+    let uuid_id = Uuid::new_v4();
+    let datetime = Utc::now();
+
+    body.id = Some(uuid_id.to_string());
+    body.completed = Some(false);
+    body.createdAt = Some(datetime);
+    body.updatedAt = Some(datetime);
+
+    let todo = body.to_owned();
+
+    vec.push(body.into_inner());
+
+    let json_response = SingleTodoResponse {
+        status: "success".to_string(),
+        data: TodoData {
+            todo: todo.into_inner(),
+        },
+    };
     Ok(Json(json_response))
 }
